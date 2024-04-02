3ed2c018adbaf92bacede003d5bc569bbebad8b4
Yogesh Bhimrao Rakate <yogesh.rakate@Yash.com>
2024-04-02 16:49:39 +0530

    Page Not Found handled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Routes, Route, useRouteError } from "react-router-dom";
 
 import Home from './components/Home';
 import Login from './components/Login';
@@ -8,8 +8,13 @@ import DisplayProduct from './components/products/DisplayProduct';
 import ListProducts from './components/products/ListProducts';
 import Products from './components/products/Products';
 import SearchProduct from './components/products/SearchProduct';
+import NotFound from './components/NotFound';
 
 function App() {
+  // const error = useRouteError();
+  // if (error) {
+  //   return <NotFound />;
+  // }
   return (
     <Router>
       <nav>
@@ -20,12 +25,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route element={<NotFound />} />
         <Route path="products" element={<Products />}>
           <Route path="search" element={<SearchProduct />} />
           <Route path="list" element={<ListProducts />} />
           <Route path="add" element={<AddProduct />} />
           <Route path=":id" element={<DisplayProduct />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>Oops! The page you are looking for does not exist.</p>
+        <p>Return to <Link to="/">Homepage</Link></p>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
